6e73c78fe8175fb00e4315a6455953af891a0017
Ariel Contreras <ariel.contreras@laerdal.com>
2025-11-15 14:15:17 +0100

    cleaning my script and removing some delays

--- script.bat
+++ script.bat
@@ -1,7 +1,4 @@
 @echo OFF
-REM reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
-REM gpupdate /force
-REM robot script.bat && robotmetrics
 
 CD "%USERPROFILE%\AppData\Local\Laerdal_Medical_A_S\InstructorApplication.exe_Url_m0k1hmqxt4hic433nxkre0orlakwh5uv\8.7.3.10398"
 DEL user.config
